0a75be8927575ee1ce072e0a5e86af59cbccf57e
Maurice Fiorani <mauricef97@gmail.com>
2020-01-30 21:01:06 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,8 @@ def oxford_comma(array)
  elsif array.length == 2
  return array.join(" and ")
  elsif array.length >=3
- new_array_last_element = "and #{array[-1]" 
+ array[-1] = "and array#{-1}"
+ return array.join(" , ")
   end
 end  
 
